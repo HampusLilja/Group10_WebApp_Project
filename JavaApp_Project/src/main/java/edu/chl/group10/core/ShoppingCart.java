c78cd74bb1d68597e6a890a825e2e2557622aac0
Mattias Herzog <herzog90@hotmail.com>
2012-10-07 14:32:13 +0200

    gjorde om shoppingcart

--- JavaApp_Project/src/main/java/edu/chl/group10/core/ShoppingCart.java
+++ JavaApp_Project/src/main/java/edu/chl/group10/core/ShoppingCart.java
@@ -4,29 +4,38 @@
  */
 package edu.chl.group10.core;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 /**
  *
  * @author Herzog
  */
 public class ShoppingCart {
-    private List<Product> purchaseOrder;
+    private HashMap<Product, Integer> purchaseOrder;
     
     public ShoppingCart(){
-        purchaseOrder = new ArrayList<>();
+        purchaseOrder = new HashMap<>();
     }
     
-    public void addProduct(Product product){
-        purchaseOrder.add(product);
+    public void addProduct(Product product, int quantity){
+        if(purchaseOrder.containsKey(product)){
+            setQuantity(product, quantity);
+        }
+        else{
+            purchaseOrder.put(product, quantity);
+        }
     }
     public void removeProduct(Product product){
         purchaseOrder.remove(product);
     }
-    public List<Product> getPurchaseOrder(){
+    public HashMap<Product, Integer> getPurchaseOrder(){
         return purchaseOrder;
     }
-    
+    public void setQuantity(Product product, int quantity){
+        purchaseOrder.put(product, getQuantity(product)+quantity);
+    }
+    public int getQuantity(Product product){
+        return purchaseOrder.get(product);
+    }
     
 }
