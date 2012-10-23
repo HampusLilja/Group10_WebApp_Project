f51ba9b36e2fefd1b8d6b5087b24babce0d639b0
tomasar <tomas.arnesson@gmail.com>
2012-10-23 13:37:35 +0200

    Updated templates, with shop dialogs etc. Javascripts for shopfunction

--- JavaApp_Project/target/generated-sources/annotations/edu/chl/group10/core/Customer_.java
+++ JavaApp_Project/target/generated-sources/annotations/edu/chl/group10/core/Customer_.java
@@ -1,22 +1,17 @@
 package edu.chl.group10.core;
 
 import edu.chl.group10.core.Address;
-import edu.chl.group10.core.ContactInfo;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-15T15:00:53")
-=======
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-15T20:17:39")
->>>>>>> javaScripting
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-23T10:54:28")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
     public static volatile SingularAttribute<Customer, Long> id;
     public static volatile SingularAttribute<Customer, String> lastName;
-    public static volatile SingularAttribute<Customer, ContactInfo> contactInfo;
+    public static volatile SingularAttribute<Customer, String> email;
     public static volatile SingularAttribute<Customer, Address> address;
     public static volatile SingularAttribute<Customer, String> firstName;
     public static volatile SingularAttribute<Customer, String> password;
