f369ce373ae6de08f81eb823228d0c9a3c12cf96
tomasar <tomas.arnesson@gmail.com>
2012-10-24 15:43:18 +0200

    Adapted design for all new pages.

--- JavaApp_Project/target/generated-sources/annotations/edu/chl/group10/core/Product_.java
+++ JavaApp_Project/target/generated-sources/annotations/edu/chl/group10/core/Product_.java
@@ -4,11 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-24T10:21:42")
-=======
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-24T10:50:22")
->>>>>>> 06cc4c46f0d3584bbfaad78aabac4f7621047b5f
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-10-24T14:58:51")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
