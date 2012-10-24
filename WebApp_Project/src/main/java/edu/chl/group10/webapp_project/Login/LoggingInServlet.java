858b1e33d80a905632f356e684f2264d43d5a9fc
Hampus Lilja <hampus.lilja@gmail.com>
2012-10-24 12:53:57 +0200

    Hashing and on password

--- WebApp_Project/src/main/java/edu/chl/group10/webapp_project/Login/LoggingInServlet.java
+++ WebApp_Project/src/main/java/edu/chl/group10/webapp_project/Login/LoggingInServlet.java
@@ -4,6 +4,9 @@ import edu.chl.group10.core.Customer;
 import edu.chl.group10.core.Group10;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -38,8 +41,9 @@ public class LoggingInServlet extends HttpServlet {
             if( typeOfAction.equals("login")) {
                 String email = request.getParameter("username");
                 String password = request.getParameter("password");
+                String hashedPassword = hash(password);
                 Customer customer = Group10.INSTANCE.getCustomerList().
-                        getByEmailAndPassword(email, password);              
+                        getByEmailAndPassword(email, hashedPassword);              
                 
                 if( customer != null ){
                     
@@ -94,4 +98,34 @@ public class LoggingInServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private String hash(String password) {
+    String salt = "GR@$OU1@P0.10&#S%^A$L*T";
+    String hash = md5(password + salt);
+        return hash;
+    }
+    public static String md5(String input) {
+         
+        String md5 = null;
+         
+        if(null == input){ 
+            return null;
+        }
+        try {
+             
+        //Create MessageDigest object for MD5
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+         
+        //Update input string in message digest
+        digest.update(input.getBytes(), 0, input.length());
+ 
+        //Converts message digest value in base 16 (hex) 
+        md5 = new BigInteger(1, digest.digest()).toString(16);
+ 
+        } catch (NoSuchAlgorithmException e) {
+ 
+            e.printStackTrace();
+        }
+        return md5;
+    }
 }
